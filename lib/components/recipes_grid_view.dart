4da9a05eb44d1393d38d2f3ca53d868c0e630e02
robsonsilv4 <robsonsilv410@gmail.com>
2021-10-30 17:20:37 -0300

    add RecipeGridView and thumbnail

---
+++ lib/components/recipes_grid_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:fooderlich/components/components.dart';
+import 'package:fooderlich/models/models.dart';
+
+class RecipesGridView extends StatelessWidget {
+  const RecipesGridView({
+    Key? key,
+    required this.recipes,
+  }) : super(key: key);
+
+  final List<SimpleRecipe> recipes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        top: 16,
+        right: 16,
+      ),
+      child: GridView.builder(
+        itemCount: recipes.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (context, index) {
+          final recipe = recipes[index];
+          return RecipeThumbnail(recipe: recipe);
+        },
+      ),
+    );
+  }
+}
