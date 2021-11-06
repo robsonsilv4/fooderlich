61250ec8da775bd195a5397730bbe5e9d4a8b397
robsonsilv4 <robsonsilv410@gmail.com>
2021-11-06 09:33:06 -0300

    add Onboarding and SplashScreen

---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final _controller = PageController();
+  final _rwColor = const Color.fromRGBO(64, 143, 77, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text('Getting started'),
+        leading: GestureDetector(
+          onTap: () => Navigator.pop(context, true),
+          child: const Icon(
+            Icons.chevron_left,
+            size: 35,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: _buildPages()),
+            _buildIndicator(),
+            _buildActionButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPages() {
+    return PageView(
+      controller: _controller,
+      children: [
+        _onboardingPageView(
+          imageProvider: const AssetImage(
+            'assets/fooderlich_assets/recommend.png',
+          ),
+          text:
+              '''Check out weekly recommended recipes and what your friends are cooking!''',
+        ),
+        _onboardingPageView(
+          imageProvider: const AssetImage(
+            'assets/fooderlich_assets/sheet.png',
+          ),
+          text: 'Cook with step by step instructions!',
+        ),
+        _onboardingPageView(
+          imageProvider: const AssetImage(
+            'assets/fooderlich_assets/list.png',
+          ),
+          text: 'Keep track of what you need to buy',
+        ),
+      ],
+    );
+  }
+
+  Widget _onboardingPageView({
+    required ImageProvider imageProvider,
+    required String text,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Image(
+              image: imageProvider,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            text,
+            style: const TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIndicator() {
+    return SmoothPageIndicator(
+      controller: _controller,
+      count: 3,
+      effect: WormEffect(
+        activeDotColor: _rwColor,
+      ),
+    );
+  }
+
+  Widget _buildActionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        MaterialButton(
+          onPressed: () {},
+          child: const Text('Skip'),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Image(
+              height: 200,
+              image: AssetImage('assets/fooderlich_assets/rw_logo.png'),
+            ),
+            Text('Initializing...'),
+          ],
+        ),
+      ),
+    );
+  }
+}
