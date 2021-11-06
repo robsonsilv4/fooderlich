7f61bf984e40e20a924121bd83d0256f1cf77661
robsonsilv4 <robsonsilv410@gmail.com>
2021-11-06 16:32:08 -0300

    update GroceryManager

--- lib/models/grocery_manager.dart
+++ lib/models/grocery_manager.dart
@@ -4,11 +4,36 @@ import 'models.dart';
 
 class GroceryManager extends ChangeNotifier {
   final _groceryItems = <GroceryItem>[];
+  int _selectedIndex = -1;
+  bool _createNewItem = false;
 
   List<GroceryItem> get groceryItems => List.unmodifiable(_groceryItems);
+  int get selectedIndex => _selectedIndex;
+  GroceryItem? get selectedGroceryItem =>
+      _selectedIndex != -1 ? _groceryItems[_selectedIndex] : null;
+  bool get isCreatingNewItem => _createNewItem;
+
+  void createNewItem() {
+    _createNewItem = true;
+    notifyListeners();
+  }
 
   void addItem(GroceryItem item) {
     _groceryItems.add(item);
+    _createNewItem = false;
+    notifyListeners();
+  }
+
+  void groceryItemTapped(int index) {
+    _selectedIndex = index;
+    _createNewItem = false;
+    notifyListeners();
+  }
+
+  void setSelectedGroceryItem(String id) {
+    final index = groceryItems.indexWhere((item) => item.id == id);
+    _selectedIndex = index;
+    _createNewItem = false;
     notifyListeners();
   }
 
@@ -19,6 +44,8 @@ class GroceryManager extends ChangeNotifier {
 
   void updateItem(GroceryItem item, int index) {
     _groceryItems[index] = item;
+    _selectedIndex = -1;
+    _createNewItem = false;
     notifyListeners();
   }
 
