7e5b51d4acc9f480d8d826129fbad2415f9b02cc
robsonsilv4 <robsonsilv410@gmail.com>
2021-11-06 09:36:49 -0300

    update Home and EmptyGoceryScreen

--- lib/screens/empty_grocery_screen.dart
+++ lib/screens/empty_grocery_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../models/models.dart';
 
 class EmptyGroceryScreen extends StatelessWidget {
   const EmptyGroceryScreen({Key? key}) : super(key: key);
@@ -36,7 +33,7 @@ class EmptyGroceryScreen extends StatelessWidget {
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(30),
               ),
-              onPressed: context.read<TabManager>().goToRecipes,
+              onPressed: () {},
               child: const Text('Browse recipes'),
             ),
           ],
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'screens.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({
+    Key? key,
+    required this.currentTab,
+  }) : super(key: key);
+
+  final int currentTab;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  static final List<Widget> _pages = [
+    const ExploreScreen(),
+    RecipesScreen(),
+    const GroceryScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlish',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        actions: [
+          _profileButton(),
+        ],
+      ),
+      body: IndexedStack(
+        index: widget.currentTab,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: widget.currentTab,
+        onTap: (index) => {},
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'To buy',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _profileButton() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16),
+      child: GestureDetector(
+        child: const CircleAvatar(
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage(
+            'assets/profile_pics/person_stef.jpeg',
+          ),
+        ),
+        onTap: () {},
+      ),
+    );
+  }
+}
