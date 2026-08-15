61e86034a3baab3c74cc624cdc9ffa7a6e0a2eea
robsonsilv4 <robsonsilv410@gmail.com>
2026-08-15 16:53:03 -0300

    chore: add test dependencies and helpers
    Add mocktail and fake_async, dart_test.yaml with unit/widget tags and
    randomized ordering, and pumpApp/test_tags helpers for the test suite.

---
+++ test/helpers/pump_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fooderlich/models/models.dart';
+import 'package:provider/provider.dart';
+
+extension PumpApp on WidgetTester {
+  Future<void> pumpApp(
+    Widget widget, {
+    AppStateManager? appStateManager,
+    ProfileManager? profileManager,
+    GroceryManager? groceryManager,
+  }) {
+    return pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => appStateManager ?? AppStateManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => profileManager ?? ProfileManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (_) => groceryManager ?? GroceryManager(),
+          ),
+        ],
+        child: MaterialApp(home: widget),
+      ),
+    );
+  }
+}
